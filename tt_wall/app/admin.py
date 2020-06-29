90838a223452eded86a3081d8fca5e8729ea67ea
trucktar <57882685+trucktar@users.noreply.github.com>
2020-06-29 06:36:07 +0300

    Register Location and Category models to admin site

--- tt_wall/app/admin.py
+++ tt_wall/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Image
+from .models import Category, Image, Location
 
 # Register your models here.
 admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Category)
