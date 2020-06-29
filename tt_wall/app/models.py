adb3e66f449bad2c9af2527859b6be5a52e1757d
trucktar <57882685+trucktar@users.noreply.github.com>
2020-06-29 06:58:03 +0300

    Extract crud methods into mixin class for all models

--- tt_wall/app/models.py
+++ tt_wall/app/models.py
@@ -1,21 +1,37 @@
 from django.db import models
 
 
-class Category(models.Model):
+class CrudMixin:
+    def save_model(self):
+        """Save model instance to the database."""
+        self.save()
+
+    def delete_model(self):
+        """Delete model instance from the database."""
+        self.delete()
+
+    def update_model(self, **kwargs):
+        """Update model instance in the database."""
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+        self.save()
+
+
+class Category(models.Model, CrudMixin):
     category_name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.category_name
 
 
-class Location(models.Model):
+class Location(models.Model, CrudMixin):
     location_name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.location_name
 
 
-class Image(models.Model):
+class Image(models.Model, CrudMixin):
     image = models.ImageField(upload_to='uploads/')
     image_name = models.CharField(max_length=30)
     image_desc = models.TextField()
@@ -27,20 +43,6 @@ class Image(models.Model):
     def __str__(self):
         """Return a string representation of the model instance."""
         return self.image_name
-
-    def save_image(self):
-        """Save image to the database."""
-        self.save()
-
-    def delete_image(self):
-        """Delete image from the database."""
-        self.delete()
-
-    def update_image(self, **kwargs):
-        """Update image in the database."""
-        for k, v in kwargs.items():
-            setattr(self, k, v)
-        self.save()
 
     @classmethod
     def get_image_by_id(cls, image_id):
