8f88fe3078173cfb712a40c9c18eb6d34aa3a3f8
trucktar <57882685+trucktar@users.noreply.github.com>
2020-06-29 05:00:22 +0300

    Create the image model and methods

--- tt_wall/app/models.py
+++ tt_wall/app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='uploads/')
+    image_name = models.CharField(max_length=30)
+    image_desc = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model instance."""
+        return self.image_name
+
+    def save_image(self):
+        """Save image to the database."""
+        self.save()
+
+    def delete_image(self):
+        """Delete image from the database."""
+        self.delete()
+
+    def update_image(self, **kwargs):
+        """Update image in the database."""
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+        self.save()
