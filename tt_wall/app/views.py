37d6057dd85adf7dcafa19da22da38e373cfac93
trucktar <57882685+trucktar@users.noreply.github.com>
2020-06-30 02:38:57 +0300

    Create view function for home page URL mapping

--- tt_wall/app/views.py
+++ tt_wall/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from tt_wall.app.models import Image
+
+
+def index(request):
+    """View function for the home page."""
+    context = {'images': Image.objects.all()}
+    return render(request, 'index.html', context=context)
