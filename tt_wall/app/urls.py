f971125d187ebde577a5bd96bf4c0f54bee9ef0d
trucktar <57882685+trucktar@users.noreply.github.com>
2020-06-30 03:19:17 +0300

    Setup storage and serving of user-uploaded files

--- tt_wall/app/urls.py
+++ tt_wall/app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from tt_wall.app import views
@@ -5,3 +7,7 @@ from tt_wall.app import views
 urlpatterns = [
     path('', views.index, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
