034f7f21ab222d2f0a969112ae6113d8491261be
trucktar <57882685+trucktar@users.noreply.github.com>
2020-06-30 08:11:18 +0300

    Define associate views for the URL mappings

--- tt_wall/app/views.py
+++ tt_wall/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from tt_wall.app.models import Image
 
@@ -7,3 +7,17 @@ def index(request):
     """View function for the home page."""
     context = {'images': Image.objects.all()}
     return render(request, 'index.html', context=context)
+
+
+def category_search(request):
+    if 'category' in request.GET and request.GET['category']:
+        category_name = request.GET.get('category')
+
+        context = {'images': Image.search_by_category(category_name)}
+        return render(request, 'index.html', context)
+    return redirect(index)
+
+
+def location_filter(request, location_name):
+    context = {'images': Image.filter_by_location(location_name)}
+    return render(request, 'index.html', context)
